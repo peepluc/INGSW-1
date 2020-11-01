5e304af497c61cf0b90a170bf9463ba7db561243
Mario <marioavolio1999@gmail.com>
2020-11-01 12:25:52 +0100

    MathYoutubeTest add

--- JUnit/MathYoutube/MathYoutubeTest/src/test/java/MathYoutubeTest/MathYoutubeTest/MathYoutubeTest.java
+++ JUnit/MathYoutube/MathYoutubeTest/src/test/java/MathYoutubeTest/MathYoutubeTest/MathYoutubeTest.java
@@ -1,6 +1,11 @@
 package MathYoutubeTest.MathYoutubeTest;
 
+import org.junit.BeforeClass;
+
 public class MathYoutubeTest 
 {
-
+	private static MathYoutube my;
+	
+	@BeforeClass
+	public static void 
 }
